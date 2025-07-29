ae506f6517615078092910f8e27501407fe89176
Prince-919 <princesharmabbu0@gmail.com>
2025-07-29 20:46:31 +0530

    add allow cross-origin frontend url

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const { dbConnect } = require("./config");
 const { appConfig } = require("./app/app");
 
 const startServer = async () => {
   const app = express();
+  const corsOptions = {
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  };
+  app.use(cors(corsOptions));
+  app.options("*", cors(corsOptions));
   await dbConnect();
   await appConfig(app);
 };
